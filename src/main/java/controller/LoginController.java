d61654cde8c4001e75a2427e1f3c12152cf62966
Bnemoussa Abdallah <benmoussaabdallah0@gmail.com>
2026-03-10 20:53:45 +0100

    UI fixing

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,14 +1,31 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Hyperlink createAccountLink;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private Hyperlink forgotPasswordLink;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private CheckBox rememberMeCheckBox;
+
 }
