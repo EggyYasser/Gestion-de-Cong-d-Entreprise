80bfe50cb8dd7d04103d0a20e59d806d657daedb
Bnemoussa Abdallah <benmoussaabdallah0@gmail.com>
2026-03-14 22:14:01 +0100

    Add Employee Form with all required fields and buttons

---
+++ src/main/java/controller/AddEmployeeController.java
@@ -0,0 +1,84 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class AddEmployeeController {
+
+    @FXML
+    private Button addEAddAnotherButton;
+
+    @FXML
+    private Button addEApproveButton;
+
+    @FXML
+    private Label addEBirthDateLabel;
+
+    @FXML
+    private DatePicker addEBirthDatePicker;
+
+    @FXML
+    private HBox addEButtonBox;
+
+    @FXML
+    private Button addECancelButton;
+
+    @FXML
+    private ComboBox<?> addEDepartmentCombo;
+
+    @FXML
+    private Label addEDepartmentLabel;
+
+    @FXML
+    private TextField addEEmailField;
+
+    @FXML
+    private Label addEEmailLabel;
+
+    @FXML
+    private TextField addEFirstNameField;
+
+    @FXML
+    private Label addEFirstNameLabel;
+
+    @FXML
+    private GridPane addEGridPane;
+
+    @FXML
+    private Label addEHeaderLabel;
+
+    @FXML
+    private Label addEHireDateLabel;
+
+    @FXML
+    private DatePicker addEHireDatePicker;
+
+    @FXML
+    private TextField addELastNameField;
+
+    @FXML
+    private Label addELastNameLabel;
+
+    @FXML
+    private TextField addEPhoneField;
+
+    @FXML
+    private Label addEPhoneLabel;
+
+    @FXML
+    private TextField addEPositionField;
+
+    @FXML
+    private Label addEPositionLabel;
+
+    @FXML
+    private VBox addEVBox;
+
+}
